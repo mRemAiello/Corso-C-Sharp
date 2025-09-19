eaffd67f1b9d26ff08c8af248b703e259449d95f
Mirko Raimondo Aiello <aiello@doneref.com>
2025-09-19 10:01:30 +0200

    Add LINQ aggregation examples

---
+++ Linq/CountSumExample.cs
@@ -0,0 +1,16 @@
+namespace LinqExamples;
+
+public static class CountSumExample
+{
+    public static void Execute()
+    {
+        List<int> numeri = new() { 4, 7, 2, 9, 12, 5, 8 };
+
+        int numeriPari = numeri.Count(n => n % 2 == 0);
+        int sommaTotale = numeri.Sum();
+
+        Console.WriteLine($"Count - Numero di valori pari: {numeriPari}");
+        Console.WriteLine($"Sum - Somma di tutti i valori: {sommaTotale}");
+        Console.WriteLine();
+    }
+}
